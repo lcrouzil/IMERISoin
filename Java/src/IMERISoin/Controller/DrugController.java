933989f5860fc983226d6b99111c22db1942b7ea
Alexis DEVLEESCHAUWER <alexis.devleeschauwer@imerir.com>
2021-04-01 08:58:05 +0200

    add all

--- Java/src/IMERISoin/Controller/DrugController.java
+++ Java/src/IMERISoin/Controller/DrugController.java
@@ -3,6 +3,9 @@ package IMERISoin.Controller;
 import IMERISoin.MainApp;
 import IMERISoin.Model.Drug;
 import IMERISoin.Model.Patient;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -12,7 +15,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import sun.net.www.protocol.http.HttpURLConnection;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ResourceBundle;
@@ -64,7 +70,37 @@ public class DrugController implements Initializable {
     }
 
     private void apiGetDrug() {
-        String url = "http://10.3.6.197:8000/"
+        String sURL = "http://10.3.6.197:8000/Patients/listMedicines";
+
+//        URL aurl = null;
+        String codeHTML = "";
+
+        try {
+            URL url = new URL(sURL);
+//            URLConnection con = aurl.openConnection();
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+
+            System.out.println(request.getResponseCode());
+            if (request.getResponseCode() == 200) {
+                JsonParser jp = new JsonParser(); //from gson
+                JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+                JsonObject rootObj = root.getAsJsonObject(); //May be an array, may be an object.
+                JsonObject medicines = rootObj.get("medicines").getAsJsonObject(); //just grab the zipcode
+
+
+
+
+                System.out.println(medicines.toString());
+            }
+
+//            con.setConnectTimeout(60000);
+
+//            System.out.println(con.getContent());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -84,9 +120,9 @@ public class DrugController implements Initializable {
     }
 
     public void refreshData() {
-
-
         drugTableView.setItems(mainApp.getDrugData());
+        apiGetDrug();
+
         nameTableColumn.setCellValueFactory(cellData -> cellData.getValue().getNameFx());
     }
 
