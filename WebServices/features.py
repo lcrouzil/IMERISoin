be57595c1a0dfce6948af2461c4074c3f2c6f458
Alexis DEVLEESCHAUWER <alexis.devleeschauwer@imerir.com>
2021-04-01 08:53:37 +0200

    fix list

--- WebServices/features.py
+++ WebServices/features.py
@@ -1,192 +1,194 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from typing import Optional
 
 import time
-from datetime import datetime 
+from datetime import datetime
 
 from database import *
+
+
 # Patients et essais cliniques
 
 
 # Définit un nouveau médicament avec un nom
-def newMedicine(medicine: int,name: str):
-    add_medicine(medicine,name)
-    if(True) :
+def newMedicine(medicine: int, name: str):
+    add_medicine(medicine, name)
+    if (True):
         code = 200
-    else :
+    else:
         code = 404
     return {"code": code}
 
 
 # Nouveau patient dans une room donnée (selon la semaine donnée ou en cours)
-def newPatient(room : int ,patientID : int,name: str,week : int):
-    add_patient(patientID,name)
-    #week = str(week)
-    #week = datetime.strptime(week, "%Y%m%d").strftime("%Y/%m/%d")
-    if(True) :
+def newPatient(room: int, patientID: int, name: str, week: int):
+    add_patient(patientID, name)
+    # week = str(week)
+    # week = datetime.strptime(week, "%Y%m%d").strftime("%Y/%m/%d")
+    if (True):
         code = 200
-    else :
+    else:
         code = 404
-    return {"code": code,"room": room,"patientID": patientID,"name": name,"week": week}
+    return {"code": code, "room": room, "patientID": patientID, "name": name, "week": week}
 
 
 # Crée ou modifie une chambre
-def newRoom(room: int, name : str):
+def newRoom(room: int, name: str):
     # Ajouter dans la database
-    if(True) :
+    if (True):
         code = 200
-    else :
+    else:
         code = 404
     return {"code": code}
 
 
 # Renvoie l'état de santé d'un patient entre "Cured"/"Stable"/"Dead"
-def patientCondition(patientID : int):
+def patientCondition(patientID: int):
     # Questionner la database
     return {"condition": condition}
 
 
 # Définit la condition du patient entre cured/stable/dead
-def newPatientCondition(patientID : int, condition : str) :
-    #Ajouter dans la database entre "cured"/"stable"/"dead"
-    if(1) :
+def newPatientCondition(patientID: int, condition: str):
+    # Ajouter dans la database entre "cured"/"stable"/"dead"
+    if (1):
         code = 200
-    else :
+    else:
         code = 404
-    return{"code": code}
+    return {"code": code}
 
 
 # Définit le médicament à donner dans la room pour la semaine indiquée (ou semaine en cours si paramètre pas donné)
-def newRoomMedicine(room : int, medicine : int, week : Optional[int]):
-    #Ajouter dans la database la medicine à donner dans la room
-    if(1) :
+def newRoomMedicine(room: int, medicine: int, week: Optional[int]):
+    # Ajouter dans la database la medicine à donner dans la room
+    if (1):
         code = 200
-    else :
+    else:
         code = 404
-    return{"code": code}
+    return {"code": code}
 
 
 # Retourne le médicament à fournir dans la room
-def roomMedicine(room : int, week : Optional[int]):
-    #Questionner la database
-    return {"room": room,"week": week,"medicine": medicine}
+def roomMedicine(room: int, week: Optional[int]):
+    # Questionner la database
+    return {"room": room, "week": week, "medicine": medicine}
 
 
 # Retourne les éléments en fonction des paramètres optionnels
-def patientStats(week : Optional[int], room : Optional[int], medicine : Optional[int], state : Optional[str]):
-    #Questionner la database
-    return {"week":week} #retourner une liste de tout
+def patientStats(week: Optional[int], room: Optional[int], medicine: Optional[int], state: Optional[str]):
+    # Questionner la database
+    return {"week": week}  # retourner une liste de tout
 
 
 # Retourne tous les médicaments disponibles
 def getMedicines():
-    tab = {}
-    for id,name in get_medicine() :
-        tab[id] = name
+    tab = []
+    for id, name in get_medicine():
+        tab.append({"patientId": id, "SS": id, "week": "no week", "name": name})
+        # tab[id] = name
 
     print(tab)
-    
-    return {"medicines": tab}
+
+    return {"list": tab}
 
 
 # Retourne la liste de tous les patients
 def getPatients():
-    #Database : pareil que getMedicines?
+    # Database : pareil que getMedicines?
     tab = {}
-    for id,name in get_patient() :
+    for id, name in get_patient():
         tab[id] = name
 
     print(tab)
-    
+
     return {"patients": tab}
+
 
 # Retourne la liste de toutes les rooms
 def getRooms():
     tab = {}
-    for id,name in get_patient() :
+    for id, name in get_patient():
         tab[id] = name
 
     print(tab)
-    
+
     return {"rooms": tab}
 
 
 # Retourne la liste de tous les robots
 def getRobots():
     tab = {}
-    for id,name in get_robot() :
+    for id, name in get_robot():
         tab[id] = name
 
     print(tab)
-    
+
     return {"robots": tab}
-
-
 
 
 # Gestion des robots et des consignes
 
 
 # Ajouter la consigne medicament pour telle room (status "to do")
-def newOrder(room: int,medicine : int):
-    #Ajouter dans la database la consigne à faire (Aller dans telle room avec tel médicament)
-    if(1) :
+def newOrder(room: int, medicine: int):
+    # Ajouter dans la database la consigne à faire (Aller dans telle room avec tel médicament)
+    if (1):
         code = 200
-    else :
+    else:
         code = 404
-    return{"code": code}
+    return {"code": code}
 
 
 # Lit la première consigne disponible
 def firstOrder(void):
-    #Questionner la database
+    # Questionner la database
     return {"order": order, "room": room, "medicine": medicine}
 
 
 # Retourne le statut d'avancement de la consigne
-def runningOrder(order : str) :
-    #Questionner la database
+def runningOrder(order: str):
+    # Questionner la database
     return {"order": order, "status": status}
 
 
 # Modifie le statut d'une consigne entre 'delivered'(medicament fourni) et 'done' (robot a la base) avec le timestamp
-def modifyStateOrder(order : str, status : str):
-    #Ajouter dans la database le nouvel état de la consigne avec le timestamp
-    if(True) :
+def modifyStateOrder(order: str, status: str):
+    # Ajouter dans la database le nouvel état de la consigne avec le timestamp
+    if (True):
         code = 200
-    else :
+    else:
         code = 404
-    return{"code": code}
+    return {"code": code}
 
 
 # Renvoie le noeud où le robot est actuellement
-def robotPosition(robot:int, node:int):
-    #Questionner la database
+def robotPosition(robot: int, node: int):
+    # Questionner la database
     return {"robot": robot_id, "node": node, "timestamp": timestamp}
 
 
 # Enregistre le noeud où est arrivé le robot + timestamp
-def robotAddNode(robot:int, node:int):
-    #Ajouter dans la database le nouveau noeud où se situe le robot
-    if(True) :
+def robotAddNode(robot: int, node: int):
+    # Ajouter dans la database le nouveau noeud où se situe le robot
+    if (True):
         code = 200
-    else :
+    else:
         code = 404
-    return{"code": code}
+    return {"code": code}
 
 
 # Historique des mouvements du robot
 def robotHistory(robot: int):
     # Questionner la base de données
-    return {"liste des mouvements node/timestamp" : afaire}
+    return {"liste des mouvements node/timestamp": afaire}
 
 
 # Crée une alerte (s'il est perdu)
 def robotLost(robot: int):
-    #Ajouter base de données
-    if(True) :
+    # Ajouter base de données
+    if (True):
         code = 200
-    else :
+    else:
         code = 404
-    return{"code": code}
+    return {"code": code}
--- WebServices/myFastAPI.py
+++ WebServices/myFastAPI.py
@@ -2,15 +2,17 @@ from typing import Optional
 
 from fastapi import FastAPI
 
-#call features
-#import features
+# call features
+# import features
 
-#for week and timestamp
+# for week and timestamp
 import time
-from datetime import datetime 
+from datetime import datetime
+
 app = FastAPI()
 
 from features import *
+
 
 @app.get("/")
 def read_root():
@@ -21,51 +23,50 @@ def read_root():
 
 @app.get("/Patients/setMedicine/{medicine}/{name}")
 # Définit un nouveau médicament avec un numéro ID et nom
-def setMedicine(medicine: int,name : str): #(medicine:nb du medoc/name:nom du medoc)
-    return newMedicine(medicine,name)
-    
-    
+def setMedicine(medicine: int, name: str):  # (medicine:nb du medoc/name:nom du medoc)
+    return newMedicine(medicine, name)
+
 
 @app.get("/Patients/addPatient/{room}/{patientID}/{name}/{week}")
 # Nouveau patient dans une room donnée (selon la semaine donnée ou en cours)
-def addPatient(room : int ,patientID : int,name: str,week : int):
-    return newPatient(room,patientID,name,week)
+def addPatient(room: int, patientID: int, name: str, week: int):
+    return newPatient(room, patientID, name, week)
 
 
 @app.get("/Patients/setRoom/{room}/{name}")
 # Crée ou modifie une chambre
-def setRoom(room: int, name : str):
-    return newRoom(room,name)
+def setRoom(room: int, name: str):
+    return newRoom(room, name)
 
 
 @app.get("/Patients/getPatientCondition/{patientID}")
 # Connaître l'état de santé d'un patient
-def getPatientCondition(patientID : int):
+def getPatientCondition(patientID: int):
     return patientCondition(patientID)
 
 
 @app.get("/Patients/setPatientCondition")
 # Définit la condition du patient entre cured/stable/dead
-def setPatientCondition(patientID : int, condition : str):
-    return newPatientCondition(patientID,condition)
+def setPatientCondition(patientID: int, condition: str):
+    return newPatientCondition(patientID, condition)
 
 
 @app.get("/Patients/setRoomMedicine")
 # Définit le médicament à donner dans la room pour la semaine indiquée (ou semaine en cours si paramètre pas donné)
-def setRoomMedicine(room : int, medicine : int, week : Optional[int]):
-    return newRoomMedicine(room,medicine,week)
+def setRoomMedicine(room: int, medicine: int, week: Optional[int]):
+    return newRoomMedicine(room, medicine, week)
 
 
 @app.get("/Patients/getRoomMedicine")
 # Retourne le médicament à fournir dans la room
-def getRoomMedicine(room : int, week : Optional[int]):
-    return roomMedicine(room,week)
+def getRoomMedicine(room: int, week: Optional[int]):
+    return roomMedicine(room, week)
 
 
 @app.get("/Patients/getStats")
 # Retourne les éléments en fonction des paramètres optionnels
-def getStats(week : Optional[int], room : Optional[int], medicine : Optional[int], state : Optional[str]):
-    return patientStats(week,room,medicine,state)
+def getStats(week: Optional[int], room: Optional[int], medicine: Optional[int], state: Optional[str]):
+    return patientStats(week, room, medicine, state)
 
 
 @app.get("/Patients/listMedicines")
@@ -77,19 +78,19 @@ def listMedicines():
 @app.get("/Patients/listPatients")
 # Retourne la liste de tous les patients
 def listPatients():
-    return getPatients() # NE FONCTIONNE PAS
+    return getPatients()  # NE FONCTIONNE PAS
 
 
 @app.get("/Patients/listRooms")
 # Retourne la liste de tous les patients
 def listRooms():
-    return getRooms() # NE FONCTIONNE PAS
+    return getRooms()  # NE FONCTIONNE PAS
 
 
 @app.get("/Patients/listRobots")
 # Retourne la liste de tous les robots
 def listRobots():
-    return getRobots() # NE FONCTIONNE PAS
+    return getRobots()  # NE FONCTIONNE PAS
 
 
 # Gestion des robots et des consignes
@@ -97,93 +98,99 @@ def listRobots():
 
 @app.get("/Robots/addOrder")
 # Ajouter la consigne medicament pour telle room (status "to do")
-def addOrder(room: int,medicine : int):
-    return newOrder(room,medicine)
-    
+def addOrder(room: int, medicine: int):
+    return newOrder(room, medicine)
+
 
 @app.get("/Robots/getOrder")
 # Lit la première consigne disponible
 def getOrder(void):
     return firstOrder()
-    
-    #ou code error si plus de consigne disponible
+
+    # ou code error si plus de consigne disponible
+
 
 @app.get("/Robots/checkOrder")
 # Retourne le statut d'avancement de la consigne
-def CheckOrder(order : str):
+def CheckOrder(order: str):
     return runningOrder(order)
-    
+
 
 @app.get("/Robots/setOrder")
 # Modifie le statut d'une consigne entre 'delivered'(medicament fourni) et 'done' (robot a la base) avec le timestamp
-def SetOrder(order : str, status : str):
-    return modifyStateOrder(order,status)
+def SetOrder(order: str, status: str):
+    return modifyStateOrder(order, status)
 
 
 @app.get("/Robots/getPosition")
 # Renvoie le noeud où le robot est actuellement
-def getPosition(robot : int):
+def getPosition(robot: int):
     return robotPosition(robot)
-    
-    
+
+
 @app.get("/Robots/setPosition")
 # Enregistre le noeud où est arrivé le robot + timestamp
-def setPosition(robot:int, node:int):
-    return robotAddNode(robot,node)
+def setPosition(robot: int, node: int):
+    return robotAddNode(robot, node)
 
 
 @app.get("/Robots/getHistory")
 # Historique des mouvements du robot
 def getHistory(robot: int):
     return robotHistory(robot)
-    
+
 
 @app.get("/Robots/alertRobot")
 # Crée une alerte (s'il est perdu)
 def alertRobot(robot: int):
     return robotLost(robot)
 
+
 @app.get("/Robots/alerts")
 def alerts():
-    #renvoie la liste des alertes en cours (non réglées)
-    return {"liste des alertes)" : afaire}
+    # renvoie la liste des alertes en cours (non réglées)
+    return {"liste des alertes)": afaire}
+
 
 @app.get("/Robots/fixAlert")
-def fixAlert(alert : str): #quel type l'alerte?
-    #alerte résolue (passe en statut réglé)
+def fixAlert(alert: str):  # quel type l'alerte?
+    # alerte résolue (passe en statut réglé)
     return code
 
-#Gestion des chemins et reservation
+
+# Gestion des chemins et reservation
 
 @app.get("/Robots/getPath/{room}")
-def getPath(room : int):
+def getPath(room: int):
     path = "RIEN"
-    if(room == 1) :
+    if (room == 1):
         path = "0F5F6F7L2L1F5R"
-    elif(room == 2):
+    elif (room == 2):
         path = "0F5F6L1L5R"
-    elif(room == 3):
+    elif (room == 3):
         path = "0F5F6F7R3R4F5L"
-    elif(room == 4):
+    elif (room == 4):
         path = "0F5F6R4R5L"
-    else :
+    else:
         path = "This room don't exist"
-    
-    #donne le chemin sous forme d'une chaîne de caractère
-    return {"room": room, "path": path }
+
+    # donne le chemin sous forme d'une chaîne de caractère
+    return {"room": room, "path": path}
 
 
 @app.get("/Robots/setPath/{room}")
-def setPath(room : int, path: str):
-    #enregistre le chemin pour aller dans la chambre et revenir
+def setPath(room: int, path: str):
+    # enregistre le chemin pour aller dans la chambre et revenir
     return {"code": 200}
 
+
 @app.get("/Robots/getNode")
-def getNode(node : int):
-    #indique si un noeud est réservé ou pas
-    return {"node":node, "booked":booked}
+def getNode(node: int):
+    # indique si un noeud est réservé ou pas
+    return {"node": node, "booked": booked}
+
 
 @app.get("/Robots/setNode")
 def setNode(node: int):
-    #réserve un noeud
-    return {"node":node,"booked":booked}
+    # réserve un noeud
+    return {"node": node, "booked": booked}
