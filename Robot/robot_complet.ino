5a8f59e6fc0a20a89ee0e7ec0f36521267280be3
Marc <marc.richard@imerir.com>
2021-04-05 22:04:25 +0200

    embelissement du code

--- Robot/robot_complet.ino
+++ Robot/robot_complet.ino
@@ -16,14 +16,13 @@
 //##                                  ##
 //######################################
 
-#define END_m1 5        // Allumer moteur Droit
-#define ENG_m2 4       // Allumer moteur gauche
-
-#define END_m3 0        // Sens du moteur droit
-#define ENG_m4 2      // Sens du moteur Gauche
-#define D5  14 // pin capteur droit
-#define D6  12 // pin capteur gauche
-#define D7 13  // trigger
+#define END_m1 5      // Allumer moteur Droit
+#define ENG_m2 4     // Allumer moteur gauche
+#define END_m3 0    // Sens du moteur droit
+#define ENG_m4 2   // Sens du moteur Gauche
+#define D5  14    // pin capteur droit
+#define D6  12   // pin capteur gauche
+#define D7 13   // trigger
 #define D8 15  // echoe
 
 //######################################
@@ -37,8 +36,6 @@ const char* cheminChambre;
 int etat=0;
 int pasloop = 0;
 char web[]="5f6r4r3r5l";
-
-
 
 // COMPTEURS
 int iteration = 1;
@@ -69,8 +66,6 @@ ICACHE_RAM_ATTR void ActiverTrigger()
   timer1_write(600000);
 }
 
-
-
 //######################################
 //##                                  ##
 //##           EcouterEcho            ##
@@ -90,8 +85,7 @@ ICACHE_RAM_ATTR void EcouterEcho()
     // CALCUL DE LA DISTANCE
     ecart_temps = temps_fin - temps_debut;
     distance = 0.000340 * (float)ecart_temps / 2.0;
-  }
-  
+  } 
 }
 
 
@@ -115,54 +109,52 @@ const int Id_Robot=01;
 //##           fonctions              ##
 //##                                  ##
 //######################################
+
  /*Fonction Avancer tout droit*/
 void goAhead(){
-      analogWrite(END_m1, 800);  // Mettre la vitesse du Moteur Droit à 700
-      analogWrite(ENG_m2, 800);  // Mettre la vitesse du Moteur Gauche à 700
-      digitalWrite(END_m3, LOW); // Sens du Moteur Droit
+      analogWrite(END_m1, 800);     // Mettre la vitesse du Moteur Droit à 700
+      analogWrite(ENG_m2, 800);    // Mettre la vitesse du Moteur Gauche à 700
+      digitalWrite(END_m3, LOW);  // Sens du Moteur Droit
       digitalWrite(ENG_m4, LOW); //Sens du Moteur Gauche
   }
 
  /*Fonction tourner à Gauche  */
 void goRight(){
-      analogWrite(END_m1, 0);    // Eteindre Moteur Droit
-      analogWrite(ENG_m2, 800);  //Mettre la vitesse du Moteur Gauche à 700
-      digitalWrite(END_m3, HIGH);// Sens du Moteur Droit
+      analogWrite(END_m1, 0);         // Eteindre Moteur Droit
+      analogWrite(ENG_m2, 800);      //Mettre la vitesse du Moteur Gauche à 700
+      digitalWrite(END_m3, HIGH);   // Sens du Moteur Droit
       //digitalWrite(ENG_m4, LOW); //Sens du Moteur Gauche
   }
   
  /*Fonction tourner à Droite  */
 void goLeft(){
-     analogWrite(END_m1, 800);  //Mettre la vitesse du Moteur Droit à 700
-     analogWrite(ENG_m2, 0);    // Eteindre Moteur Gauche
+     analogWrite(END_m1, 800);      //Mettre la vitesse du Moteur Droit à 700
+     analogWrite(ENG_m2, 0);       // Eteindre Moteur Gauche
      //digitalWrite(END_m3, LOW); // Sens du Moteur Droit
-     digitalWrite(ENG_m4, HIGH);//Sens du Moteur Gauche
+     digitalWrite(ENG_m4, HIGH); //Sens du Moteur Gauche
   }
 
  /*Fonction arreter le Robot */
 void stopRobot(){  
-      digitalWrite(END_m1, LOW); // Eteindre Moteur Droit
-      //digitalWrite(END_m3, LOW); // Sens du Moteur Droit
-      digitalWrite(ENG_m2, LOW); // Eteindre Moteur Gauche
+      digitalWrite(END_m1, LOW);      // Eteindre Moteur Droit
+      //digitalWrite(END_m3, LOW);   // Sens du Moteur Droit
+      digitalWrite(ENG_m2, LOW);    // Eteindre Moteur Gauche
       //digitalWrite(ENG_m4, LOW); // Sens du Moteur Gauche
   }
 
 //######################################
 //##                                  ##
-//##           réseaux demande ordre  ##
+//##      réseaux demande ordre       ##
 //##                                  ##
 //######################################
+
 void getOrder()
 {
   // Creation du client sécurisé :
   WiFiClient httpsClient;    //Declare object of class WiFiClient
   //Serial.println(host);
-
-
- 
   httpsClient.setTimeout(15000); // 15 Seconds
   delay(1000);
-
 
   // Tentative de connexion :
   Serial.print("HTTPS Connecting");
@@ -176,34 +168,28 @@ void getOrder()
   
   if(r==30) {Serial.println("Connection failed");}
   else {Serial.println("Connected to web");}
-
-
+  
   // Envoie d'une intersection :
   int intersection = 5;
   String STR_intersection;
   STR_intersection = String(intersection);   //String to interger conversion
-
-
+  
   // Construction de la requete :
   String Link;
   Link = "/";
-
   Serial.print("requesting URL: ");
   //Serial.println(host+Link);
-
-
+  
   // Envoie requete chemin
   httpsClient.print(String("GET ")+ "/Robots/getOrder" + " HTTP/1.1\r\n" + "Host: 10.3.6.197" +":"+ httpsPort  + "\r\n" + "Connection: close\r\n\r\n");
   Serial.println("demande ordre");
  
-
   // Reception du message :
   while (httpsClient.connected())
   {
     delay(10);
     
   }
-
 
   // Impression du message :  
   Serial.println("reply was:");
@@ -217,8 +203,7 @@ void getOrder()
   Serial.println("==========");
   Serial.println("closing connection");
 
-      // déserialisation
-      
+  // déserialisation  
   StaticJsonDocument<200> order;
   DeserializationError error = deserializeJson(order, line); //(document, input)
 
@@ -230,31 +215,28 @@ void getOrder()
   }
   int ordreChambre = order["room"];
 
-//verif 
-  
+  //verif 
   Serial.println(ordreChambre);
-  
   Serial.println(line);
   numChambre= ordreChambre;
   // Attente :
   delay(2000);  //GET Data at every 2 seconds
 }
+
 //######################################
 //##                                  ##
-//##         réseaux demande chemin   ##
+//##     réseaux demande chemin       ##
 //##                                  ##
 //######################################
+
 void getPath()
 { 
   // Creation du client sécurisé :
   WiFiClient httpsClient;    //Declare object of class WiFiClient
   //Serial.println(host);
 
-
- 
   httpsClient.setTimeout(15000); // 15 Seconds
   delay(1000);
-
 
   // Tentative de connexion :
   Serial.print("HTTPS Connecting");
@@ -269,26 +251,21 @@ void getPath()
   if(r==30) {Serial.println("Connection failed");}
   else {Serial.println("Connected to web");}
 
-
   // Envoie d'une intersection :
   int intersection = 5;
   String STR_intersection;
   STR_intersection = String(intersection);   //String to interger conversion
 
-
   // Construction de la requete :
   String Link;
   Link = "/";
-
   Serial.print("requesting URL: ");
   //Serial.println(host+Link);
-
 
   // Envoie requete chemin
   httpsClient.print(String("GET ")+ "/Robots/getPath/" + numChambre + " HTTP/1.1\r\n" + "Host: 10.3.6.197" +":"+ httpsPort  + "\r\n" + "Connection: close\r\n\r\n");
   //Serial.println("demande chemin");
  
-
   // Reception du message :
   while (httpsClient.connected())
   {
@@ -435,26 +412,23 @@ void setup()
   // INITIALISATION DE LA VARIABLE :
   distance = 0;
 
-
   // ACTIVATION AUTOMATIQUE DU CAPTEUR ULTRASON :
   timer1_attachInterrupt( ActiverTrigger );
   timer1_enable( TIM_DIV16, TIM_EDGE, TIM_SINGLE );
   timer1_write(600000);
 
   WiFi.mode(WIFI_STA);        //Only Station No AP, This line hides the viewing of ESP as wifi hotspot
-  
   WiFi.begin(ssid, password);     //Connect to your WiFi router
   Serial.println("");
-
   Serial.print("Connecting");
+  
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
   }
-
-
+  
   //If connection successful show IP address in serial monitor
   Serial.println("");
   Serial.print("Connected to ");
@@ -472,19 +446,18 @@ void setup()
 
 void loop()
 {
-     //######################################
+    //######################################
     //##                                  ##
     //##           robot                  ##
     //##                                  ##
     //######################################
+    
   int captD = digitalRead(D5); // Lire la valeur du capteur droit
   int captG = digitalRead(D6); // Lire la valeur du capteur gauche
  
   switch (etat) 
   {
-    
     //##################################### ETAT INITIAL : AVANCE #####################################
-    
     case 0:
     goAhead();
     Serial.println(distance);
@@ -497,15 +470,8 @@ void loop()
       etat=1;
     }
     break;
-    
-    //###################################################################################################
-    
-    
-
-    
-    //######################################### ETAT 1: suivie de ligne #########################################
-    
-    // CHANGE D'ETAT SI UN DES CAPTEUR DETECTE LA LIGNE
+    //###################################################################################################  
+    //################################### ETAT 1: suivie de ligne #######################################
     case 1:
     goAhead(); // avance tant qu'un des 2 capteurs ne detecte rien
     while(distance<0.5)
@@ -525,15 +491,9 @@ void loop()
       etat=2;
     }
     break;
-    
-
-    //##################################### ETAT 2: intersection #####################################
-    
-    
-    
+    //################################################################################################### 
+    //##################################### ETAT 2: intersection ########################################
     case 2:
-    
-    
     //dir = cheminChambre[iteration];  
     //noeud = cheminChambre[pos];
     dir = web[iteration];  
@@ -543,38 +503,31 @@ void loop()
     {
       stopRobot();
     }
-   
     if ( dir== 'f')
     {
       stopRobot();
       delay(2000);
       etat = 0;
     }
- 
     if( dir == 'l')
     {
       stopRobot();
       delay(2000);
       goLeft();
       delay(500);
-      
       etat = 1;
     }
-   
     if( dir=='r')
     {
       stopRobot();
       delay(2000);
       goRight();
       delay(500);
-      
       etat = 1;
     }
-    
     iteration = iteration + 2;// permet d'avancer sur la chaine en prenant en compte que les ordres
     pos = pos + 2;            // permet d'avancer sur la chaine en prenant en compte que les noeuds 
-   
     break;
-   
+    //################################################################################################### 
     }
 }
